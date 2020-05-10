0202b253f65af345c99a5362805c9c812bbcf8fe
UtkarshK786 <ukushwaha409@gmail.com>
2020-05-10 16:56:44 +0530

    Initial commit

---
+++ android/src/com/glider_ride/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.glider_ride.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.glider_ride.game.GliderRide;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GliderRide(), config);
+	}
+}
